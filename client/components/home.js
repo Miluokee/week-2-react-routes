04fa0495835a83ad055958cb8931b59eead10cd7
Miluokee <tim.srbk@gmail.com>
2020-09-26 20:35:54 +0300

    trying

--- client/components/home.js
+++ client/components/home.js
@@ -7,7 +7,7 @@ const Home = () => {
       <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          {/* add your routes here */}
+          Fucking trying to push
         </div>
       </div>
     </div>
