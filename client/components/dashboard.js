cf678306ca1c7c2e9e48f3b195b9356b92b22124
Miluokee <tim.srbk@gmail.com>
2020-09-27 16:43:34 +0300

    one more time

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -7,11 +7,14 @@ const Dashboard = () => {
     <div>
       <Header />
       <div className="flex items-center justify-center h-screen">
-        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10" id="title">
+        <div
+          id="title"
+          className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10"
+        >
           Dashboard
-          <Link to="/dashboard/profile/239c8d29-7ad6-4d56-a4e5-1dcc9abd969e"> Go To Profile </Link>
-          <Link to="/dashboard/main"> Go To Main </Link>
         </div>
+        <Link to="/dashboard/profile/239c8d29-7ad6-4d56-a4e5-1dcc9abd969e"> Go To Profile </Link>
+        <Link to="/dashboard/main"> Go To Main </Link>
       </div>
     </div>
   )
