ddedbe30e00fcf63d80086d62d6daf0db63e710c
Miluokee <tim.srbk@gmail.com>
2020-09-27 23:07:17 +0300

    new fix

--- client/components/home.js
+++ client/components/home.js
@@ -10,10 +10,7 @@ const Home = () => {
     <div>
       <Header />
       <div className="flex items-center justify-center h-screen">
-        <div
-          className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10"
-          id="title"
-        >
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <Route exact path="/dashboard/" component={() => <Dashboard />} />
           <Route exact path="/dashboard/main" component={() => <Main />} />
           <Route exact path="/dashboard/profile/:username" component={() => <Profile />} />
